f6081c7929151e1fe4d2952e06d395482f9296a8
Wong <wongk13@medtronic.com>
2017-03-14 10:09:20 -0500

    Checkpoint 16 jQuery Landing Page

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,35 +1,25 @@
-var points = document.getElementsByClassName('point');
-
-var animatePoints = function(points) {
-
-    //var points = document.getElementsByClassName('point');
-    var revealPoints = function(index){
-        points[index].style.opacity = 1;
-        points[index].style.transform = "scaleX(1) translateY(0)";
-        points[index].style.msTransform = "scaleX(1) translateY(0)";
-        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+var animatePoints = function() {
+    var revealPoint = function(){
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        })    
     };
-    for (var i = 0; i < points.length; i++){
-        revealPoints(i);
-    }
-    
+    $.each($('.point'), revealPoint);
 };
 
-//animatePoints();
-
-window.onload = function(){
-    if (window.innerHeight > 950) {
-         animatePoints(points);
+$(window).load(function() {
+    if ($(window).height() > 950) {
+         animatePoints();
      }
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight +200;
-    window.addEventListener('scroll',function(event){
-        console.log("Window innerheight: "+window.innerHeight);
-        console.log("Current offset from the top is: "+sellingPoints.getBoundingClientRect().top+ " pixels");
-        console.log("scrollDistance = "+ (sellingPoints.getBoundingClientRect().top - window.innerHeight +200) );
-        console.log("scrollTop: "+ document.documentElement.scrollTop);
-        if (document.documentElement.scrollTop || document.body.scrollTop > scrollDistance) {
-            animatePoints(points);
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+    $(window).scroll(function(event) {
+        //console.log("Window innerheight: "+window.innerHeight);
+        //console.log("Current offset from the top is: "+sellingPoints.getBoundingClientRect().top+ " pixels");
+        //console.log("scrollDistance = "+ (sellingPoints.getBoundingClientRect().top - window.innerHeight +200) );
+        //console.log("scrollTop: "+ document.documentElement.scrollTop);
+        if ($(window).scrollTop() >= scrollDistance) { 
+            animatePoints();
         }
     });
-}
+});
